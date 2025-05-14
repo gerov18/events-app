d8cd75638da743f67351297cd9d1203f55432f8d
Borislav Gerov <bgerov18@gmail.com>
2025-05-14 22:08:06 +0300

    fix: setup basic environment

--- react/event-app/src/App.tsx
+++ react/event-app/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import Home from './Views/Home/Home';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Home />
     </>
   );
 }
---
+++ react/event-app/src/Views/Home/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the Events App</h1>
+      <p>Discover and manage your events easily!</p>
+    </div>
+  );
+};
+
+export default Home;
